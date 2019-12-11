7966544247a990bad4f3d5c2fb15780f4b70d44f
Lucas Inocente <olucassanchez@gmail.com>
2019-12-11 09:59:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DEVELOP!
+          DEVELOP!!!!
         </p>
         <a
           className="App-link"
